d8c1a84c06affbb446a8913e2324201774f7caae
Watchara Pongsri <giffyp@hotmail.com>
2020-06-23 00:40:00 +0700

    refactor

--- solar-monitoring/src/App.tsx
+++ solar-monitoring/src/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import Monitoring from './components/monitoring/Monitoring'
+import Monitoring from './components/monitoring/Monitoring';
 import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-
-      <Monitoring
-      />
-
+    <div className='App'>
+      <Monitoring />
     </div>
   );
-}
+};
 
 export default App;
